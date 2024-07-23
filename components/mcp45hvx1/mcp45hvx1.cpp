af7be54700fbd0d616bc46a9fdea4e89fd0e9e03
danieltill <info@danieltill.de>
2024-07-24 00:19:36 +0200

    initial code sceleton for testing

--- components/mcp45hvx1/mcp45hvx1.cpp
+++ components/mcp45hvx1/mcp45hvx1.cpp
@@ -6,7 +6,7 @@ namespace mcp45hvx1 {
 
 static const char *const TAG = "mcp45hvx1.output";
 
-void X9cOutput::test_wlat(int change_amount) {
+void Mcp45hvx1Output::test_wlat(int change_amount) {
   if (change_amount == 0) {
     return;
   }
@@ -17,7 +17,7 @@ void X9cOutput::test_wlat(int change_amount) {
   }
 }
 
-void X9cOutput::setup() {
+void Mcp45hvx1Output::setup() {
   ESP_LOGCONFIG(TAG, "Setting up MCP45HVX1 Potentiometer with initial value of %f", this->initial_value_);
 }
 
@@ -25,7 +25,7 @@ void X9cOutput::write_state(float state) {
   return;
 }
 
-void X9cOutput::dump_config() {
+void Mcp45hvx1Output::dump_config() {
   ESP_LOGCONFIG(TAG, "MCP45HVX1 Potentiometer Output:");
   LOG_PIN("  SHDN Pin: ", this->shdn_pin_);
   LOG_PIN("  WLAT Pin: ", this->wlat_pin_);
