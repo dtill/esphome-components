6389b395f88a92ca3007f8058dec5f54299511fe
danieltill <info@danieltill.de>
2024-07-24 01:40:03 +0200

    initial code sceleton for testing

--- components/mcp45hvx1/mcp45hvx1.h
+++ components/mcp45hvx1/mcp45hvx1.h
@@ -17,6 +17,7 @@ class Mcp45hvx1Output : public output::FloatOutput, public Component, public i2c
   void set_wlat_pin(GPIOPin *pin) { wlat_pin_ = pin; }
 
  protected:
+  void write_state(float state) override;
   float initial_value_;
   GPIOPin *shdn_pin_{nullptr};
   GPIOPin *wlat_pin_{nullptr};
