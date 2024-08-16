4343defac52efe4ebca80abe415dd59282a392f5
danieltill <info@danieltill.de>
2024-08-16 22:43:52 +0200

    MCP41HV51 SPI init

--- components/mcp41hvx1/mcp41hvx1.cpp
+++ components/mcp41hvx1/mcp41hvx1.cpp
@@ -8,7 +8,7 @@ static const char *TAG = "mcp41hvx1";
 
 void Mcp41hvx1Output::setup() {
   ESP_LOGCONFIG(TAG, "Setting up MCP41HVX1...");
-  this->spi_setup();
+  this->spi_setup();  // Setup the SPI device
 }
 
 void Mcp41hvx1Output::write_state(float state) {
@@ -26,35 +26,30 @@ void Mcp41hvx1Output::write_state(float state) {
 void Mcp41hvx1Output::dump_config() {
   ESP_LOGCONFIG(TAG, "Mcp41hvx1_spi:");
   LOG_PIN("  CS Pin: ", this->cs_pin_);
-}
-
-float Mcp41hvx1Output::read_wiper() {
-  if (this->is_failed()) {
-    ESP_LOGE(TAG, "SPI device not initialized");
-    return -1.0f;  // Return an invalid value
-  }
-
-  uint8_t position = this->wiper_get_position();
-
-  float state = static_cast<float>(position) / 255.0;  // Convert to float between 0 and 1
-  ESP_LOGI(TAG, "Read MCP45HVX1 wiper value: %d", static_cast<int>(state * 255));
-  return state;
+  ESP_LOGCONFIG(TAG, "  Data Rate: %d", this->data_rate_);
+  ESP_LOGCONFIG(TAG, "  Mode: %d", this->mode_);
+  ESP_LOGCONFIG(TAG, "  Bit Order: %d", this->bit_order_);
 }
 
 void Mcp41hvx1Output::wiper_set_position(uint8_t position) {
-  this->enable();
-  this->spi_->transfer(0x00);       // The command for wiper set position
-  this->spi_->transfer(position);   // The value for the wiper position
-  this->disable();
+  this->enable();  // Assert CS low to start communication
+
+  this->write_byte(0x00);  // The command for wiper set position
+  this->write_byte(position);  // The value for the wiper position
+
+  this->disable();  // Assert CS high to end communication
 }
 
 uint8_t Mcp41hvx1Output::wiper_get_position() {
-  uint16_t response = 0;
-  this->enable();
-  response = this->spi_->transfer16(0x0C00);        // requesting the wiper position
-  this->disable();
-  return static_cast<uint8_t>(response & 0xff);     // The wiper position is in the low order byte
+  this->enable();  // Assert CS low to start communication
+
+  this->write_byte(0x0C);  // The command for requesting the wiper position
+  uint8_t position = this->read_byte();  // Read the wiper position
+
+  this->disable();  // Assert CS high to end communication
+
+  return position;
 }
 
-}  // namespace mcp45hvx1
+}  // namespace mcp41hvx1
 }  // namespace esphome
