fbb8825e6b2697b6056058c30eb7f6d87833cb4b
danieltill <info@danieltill.de>
2025-01-16 13:36:02 +0100

    restructure the whole compponent and add sensor and output component files

--- components/gdoor/gdoor_component.h
+++ components/gdoor/gdoor_component.h
@@ -8,9 +8,9 @@ namespace gdoor_esphome {
 class GdoorComponent : public Component {
  public:
   // Methods for setting the pins and sensitivity.
-  void set_tx_pin(int tx_pin);
-  void set_tx_en_pin(int tx_en_pin);
-  void set_rx_pin(int rx_pin);
+  void set_tx_pin(GPIOPin *tx_pin);
+  void set_tx_en_pin(GPIOPin *tx_en_pin);
+  void set_rx_pin(GPIOPin *rx_pin);
   void set_rx_sens(float rx_sens);
 
   void setup() override;
@@ -19,14 +19,14 @@ class GdoorComponent : public Component {
   void set_last_rx_data(GDOOR_DATA* &data) { this->last_rx_data_ = data; }
   GDOOR_DATA* get_last_rx_data() { return this->last_rx_data_; }
 
-  int tx_pin() const { return tx_pin_; }
-  int tx_en_pin() const { return tx_en_pin_; }
-  int rx_pin() const { return rx_pin_; }
+  GPIOPin* tx_pin() const { return tx_pin_; }
+  GPIOPin* tx_en_pin() const { return tx_en_pin_; }
+  GPIOPin* rx_pin() const { return rx_pin_; }
 
  protected:
-  int tx_pin_{-1};
-  int tx_en_pin_{-1};
-  int rx_pin_{-1};
+  GPIOPin *tx_pin_{nullptr};
+  GPIOPin *tx_en_pin_{nullptr};
+  GPIOPin *rx_pin_{nullptr};
   float rx_sens_{-1};
   GDOOR_DATA* last_rx_data_{nullptr};
 };
