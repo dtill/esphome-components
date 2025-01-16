e3c4d865b7c1b6d81c29d1e5af1d7fa1bcd7f3d8
danieltill <info@danieltill.de>
2025-01-16 18:42:10 +0100

    restructure the whole compponent and add sensor and output component files

--- components/gdoor/text_sensor/__init__.py
+++ components/gdoor/text_sensor/__init__.py
@@ -20,4 +20,5 @@ async def to_code(config):
     parent = await cg.get_variable(config["gdoor_id"])
     var = cg.new_Pvariable(config[cv.GenerateID()])
     await text_sensor.register_text_sensor(var, config)
+    await cg.register_component(var, config)
     cg.add(var.set_parent(parent))
