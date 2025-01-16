3599d6fc2ff04ba88a7b0b485495157db3e5bb67
danieltill <info@danieltill.de>
2025-01-16 20:01:09 +0100

    restructure the whole compponent and add sensor and output component files

--- components/gdoor/text_sensor/__init__.py
+++ components/gdoor/text_sensor/__init__.py
@@ -6,7 +6,6 @@ from .. import DOMAIN, GdoorComponent, gdoor_esphome_ns
 
 CODEOWNERS = ["@dtill"]
 DEPENDENCIES = [DOMAIN]
-MULTI_CONF = True
 
 # Define the text sensor class for gdoor
 GDoorBusMessage = gdoor_esphome_ns.class_("GDoorBusMessage", text_sensor.TextSensor, cg.Component)
