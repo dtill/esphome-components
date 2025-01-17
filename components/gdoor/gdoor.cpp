bf46b5668b6bbae4efbf58e9a1c9c1367495932b
danieltill <info@danieltill.de>
2025-01-18 00:40:19 +0100

    update gdoor_data

--- components/gdoor/gdoor.cpp
+++ components/gdoor/gdoor.cpp
@@ -63,7 +63,7 @@ namespace GDOOR {
     * @param hex string data without 0x prefix
     */
     void send(String str) {
-        ESP_LOGD(TAG, "GDoor call sending function GDOOR_TX::send");
+        ESP_LOGD(TAG, "GDoor call sending function GDOOR_TX::send with payload: %s", str.c_str());
         GDOOR_TX::send(str);
 
     }
