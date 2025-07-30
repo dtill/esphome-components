f3727c811bcf667d2b356f29433390df1167b70f
danieltill <info@danieltill.de>
2025-07-30 15:48:25 +0200

    debug timer

--- components/gdoor/gdoor_rx.cpp
+++ components/gdoor/gdoor_rx.cpp
@@ -58,16 +58,18 @@ namespace GDOOR_RX {
     * If this timer fires, the rx 60kHz pulse-train stopped,
     * so we should read out how many pulses we got for this bit (to decide 1 or 0)
     */
-    void IRAM_ATTR isr_timer_bit_received() {
-        if (bitcounter > MAX_WORDLEN*9) {
-            bitcounter = 0;
-        }
-        counts[bitcounter] = isr_cnt;
-
-        isr_cnt = 0;
-        bitcounter = bitcounter + 1;
-        timerStop(timer_bit_received);
-    }
+    //void IRAM_ATTR isr_timer_bit_received() {
+    //    if (bitcounter > MAX_WORDLEN*9) {
+    //        bitcounter = 0;
+    //    }
+    //    counts[bitcounter] = isr_cnt;
+    //
+    //    isr_cnt = 0;
+    //    bitcounter = bitcounter + 1;
+    //    timerStop(timer_bit_received);
+    //}
+    static volatile uint32_t dbg_cnt = 0;
+    void IRAM_ATTR isr_timer_bit_received() { dbg_cnt++; }
 
     /*
     * If this timer fires, rx bit stream is over
