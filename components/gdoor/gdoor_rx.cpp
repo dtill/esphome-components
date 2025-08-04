e76e77f35066f98a6371632cadef13fc21a271fe
danieltill <info@danieltill.de>
2025-08-04 18:38:05 +0200

    debug timer Api initialisation

--- components/gdoor/gdoor_rx.cpp
+++ components/gdoor/gdoor_rx.cpp
@@ -1,4 +1,21 @@
-include "defines.h"
+/*
+ * This file is part of the GDoor distribution (https://github.com/gdoor-org).
+ * Copyright (c) 2024 GDoor authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "defines.h"
 #include "gdoor_rx.h"
 #include "gdoor_data.h"
 #include "esphome/core/log.h"
