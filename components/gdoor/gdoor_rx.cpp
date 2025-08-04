0e2b12d79422ff1fc370664fe6cdf7d02f7ab118
danieltill <info@danieltill.de>
2025-08-04 17:34:36 +0200

    debug timer Api initialisation

--- components/gdoor/gdoor_rx.cpp
+++ components/gdoor/gdoor_rx.cpp
@@ -23,23 +23,26 @@ namespace GDOOR_RX {
 
     void enable() {
         attachInterrupt(pin_rx, isr_extint_rx, FALLING);
-        ESP_LOGD(TAG, "RX interrupt enabled on FALLING edge.");
     }
     void disable() {
         detachInterrupt(pin_rx);
-        ESP_LOGD(TAG, "RX interrupt disabled.");
     }
+     void reset() {
+        edge_pos = -1;
+        rx_state = 0;
+    }
+
 
     void setup(uint8_t rxpin) {
         pin_rx = rxpin;
         pinMode(pin_rx, INPUT_PULLUP);
-        edge_pos = -1;
-        ESP_LOGI(TAG, "FINAL RX: Setup on pin %d, using safe emulation of original pulse counting logic.", rxpin);
+        reset();
         enable();
     }
 
     void loop() {
         if (edge_pos < 0) return;
+
         if ((micros() - edge_timings[edge_pos]) > 5000) {
             noInterrupts();
             int32_t local_pos = edge_pos;
@@ -49,8 +52,6 @@ namespace GDOOR_RX {
             interrupts();
 
             if (local_pos < 1) return;
-
-            ESP_LOGD(TAG, "Message complete with %d pulses. Reconstructing counts...", local_pos + 1);
 
             uint16_t counts[MAX_WORDLEN * 9] = {0};
             uint8_t bit_idx = 0;
@@ -71,27 +72,9 @@ namespace GDOOR_RX {
                 }
             }
 
-            char debug_buffer[256];
-            int offset = 0;
-            offset += snprintf(debug_buffer, sizeof(debug_buffer), "Reconstructed Counts: [");
-            for(int i=0; i<bit_idx; i++) {
-                if(offset < 240) offset += snprintf(debug_buffer+offset, sizeof(debug_buffer)-offset, "%d, ", counts[i]);
-            }
-            snprintf(debug_buffer+offset, sizeof(debug_buffer)-offset, "]");
-            ESP_LOGD(TAG, "%s", debug_buffer);
-
             if (retval.parse(counts, bit_idx)) {
-                // #######################################################
-                // ## HIER IST DIE TATSÄCHLICHE, FUNKTIONIERENDE KORREKTUR ##
-                // #######################################################
-                char hex_buffer[MAX_WORDLEN * 2 + 1];
-                retval.to_hex(hex_buffer);
-                ESP_LOGI(TAG, "Parse SUCCESS! -> HEX: %s", hex_buffer);
-                // #######################################################
-
+                ESP_LOGVV(TAG, "Gira RX was successfully parsed");
                 rx_state |= FLAG_DATA_READY;
-            } else {
-                ESP_LOGW(TAG, "Parse FAILED! (bit_idx=%d)", bit_idx);
             }
         }
     }
