18e5b03157b39aaa4e416cb5064e325ad9a575d8
danieltill <info@danieltill.de>
2025-10-31 23:36:20 +0100

    add systa_reader compact status_code

--- components/systa_reader/compact.cpp
+++ components/systa_reader/compact.cpp
@@ -25,7 +25,8 @@ void CompactDecoder::on_fc_frame(const std::vector<uint8_t>& frame,
   snprintf(ts, sizeof(ts), "%02d.%02d %02d:%02d", day, month, hour, minute);
   r_.pub_compact_timestamp(ts);
 
-  uint8_t status = payload.size() > 20 ? payload[20] : 0; // Status (Byte)
+  uint8_t status_raw = payload.size() > 20 ? payload[20] : 0; // Status (Byte)
+  uint8_t status_code = uint8_t((status_raw/10) * 16 + (status_raw%10));
 
   // Werte gemäß Vorlage
   float ta = u16(8) / 10.0f;
@@ -45,10 +46,10 @@ void CompactDecoder::on_fc_frame(const std::vector<uint8_t>& frame,
   r_.pub_compact_ti_s(ti_s);
   r_.pub_compact_tv_s (tv_s);
   r_.pub_compact_two_s (two_s);
-  r_.pub_compact_status_code(status);
+  r_.pub_compact_status_code(status_code);
 
   ESP_LOGI(TAG_ESP, "COMPACT: TA=%.1f TWO=%.1f FA TV=%.1f FA TR=%.1f TI=%.1f TI SOLL=%.1f TV SOLL=%.1f TWO SOLL=%.1f STATUS=%02X",
-           ta,two,fa_tv,fa_tr,ti,ti_s,tv_s,two_s,status);
+           ta,two,fa_tv,fa_tr,ti,ti_s,tv_s,two_s,status_code);
 }
 
 }  // namespace systa_reader
