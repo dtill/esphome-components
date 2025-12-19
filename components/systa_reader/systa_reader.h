f69e259a95af4cb2fb44c78099f00dbef40bbebf
danieltill <info@danieltill.de>
2025-12-19 15:16:05 +0100

    add palletti_ii

--- components/systa_reader/systa_reader.h
+++ components/systa_reader/systa_reader.h
@@ -378,7 +378,7 @@ class SystaReader : public uart::UARTDevice, public Component {
   text_sensor::TextSensor *espresso_timestamp_{nullptr};
 
   // decoder instances
-  Palletti2Decoder *espresso_{nullptr};
+  Palletti2Decoder *palletti_ii_{nullptr};
   // PALLETTI-II sensor pointers
   sensor::Sensor *palletti_ii_ta_{nullptr};
   sensor::Sensor *palletti_ii_two_{nullptr};
